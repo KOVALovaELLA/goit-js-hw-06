ac75e0cf5020f5fbdd0be2043865b7a88394722c
KOVALovaELLA <ellakovaleva2006@gmail.com>
2023-10-26 18:45:44 +0200

    change 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,21 +1,52 @@
+
+
 const loginForm = document.querySelector('.login-form');
 
 loginForm.addEventListener('submit', function (event) {
   event.preventDefault(); 
 
-  const emailInput = loginForm.elements.email;
-  const passwordInput = loginForm.elements.password;
+  const formData = {};
+  const formElements = loginForm.elements;
 
-  if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
-    alert('Всі поля повинні бути заповнені');
+  for (let i = 0; i < formElements.length; i++) {
+    if (formElements[i].type !== 'submit') {
+      const value = formElements[i].value.trim();
+      if (value === '') {
+        alert('Всі поля повинні бути заповнені');
+        return; 
+      }
+      formData[formElements[i].name] = value;
+    }
+  }
+
+  if (!formData.email || !formData.password) {
+    alert('Email і пароль повинні бути заповнені');
   } else {
-    const loginData = {
-      email: emailInput.value,
-      password: passwordInput.value,
-    };
-
-    console.log(loginData);
+    console.log(formData);
 
     loginForm.reset(); 
   }
 });
+
+
+//const loginForm = document.querySelector('.login-form');
+
+//loginForm.addEventListener('submit', function (event) {
+//  event.preventDefault(); 
+
+ // const emailInput = loginForm.elements.email;
+ // const passwordInput = loginForm.elements.password;
+
+//  if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+//    alert('Всі поля повинні бути заповнені');
+ // } else {
+ //   const loginData = {
+ //     email: emailInput.value,
+ //     password: passwordInput.value,
+ //   };
+
+ //   console.log(loginData);
+
+//    loginForm.reset(); 
+//  }
+//});
